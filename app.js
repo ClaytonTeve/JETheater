4af5330fe89d8836f22d4d9424f34738a965e9ba
Clayton Tevebaugh <teeveebaw@gmail.com>
2015-10-30 12:54:37 -0700

    Slow added to jQuery show and hide
    Added the slow feature to add a level of dimension to the featured
    musicals section.

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 $(".musical_left").on('click', function(){
-   $(".hidden-box1").show();
-   $("#featured_musicals").hide();
+   $(".hidden-box1").show('slow');
+   $("#featured_musicals").hide('slow');
 });
 
 $(".musical_right").on('click', function(){
-   $(".hidden-box2").show();
-   $("#featured_musicals").hide();
+   $(".hidden-box2").show('slow');
+   $("#featured_musicals").hide('slow');
 });
 
 $('.exit-button').on('click', function(){
-   $('.hidden-box1, .hidden-box2').hide();
-   $("#featured_musicals").show();
+   $('.hidden-box1, .hidden-box2').hide('slow');
+   $("#featured_musicals").show('slow');
 });
 
